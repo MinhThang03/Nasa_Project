a600e17cdb1fd733ff0d7b8e873699a437477698
MinhThang03 <minhhoangthang75@gmail.com>
2022-11-22 23:09:07 +0700

    add: connect mongodb

--- Nasa_19110462/client/src/hooks/requests.js
+++ Nasa_19110462/client/src/hooks/requests.js
@@ -1,55 +1,49 @@
-const API_URL = 'http://localhost:8000'
+const API_URL = 'v1';
 
+// Load planets and return as JSON.
 async function httpGetPlanets() {
-  const response = await fetch(`${API_URL}/planets`)
-  return await response.json()
-  // TODO: Once API is ready.
-  // Load planets and return as JSON.
+  const response = await fetch(`${API_URL}/planets`);
+  return await response.json();
 }
 
+// Load launches, sort by flight number, and return as JSON.
 async function httpGetLaunches() {
   const response = await fetch(`${API_URL}/launches`);
   const fetchedLaunches = await response.json();
-  return fetchedLaunches.sort((a,b) => {
+  return fetchedLaunches.sort((a, b) => {
     return a.flightNumber - b.flightNumber;
   });
-  // TODO: Once API is ready.
-  // Load launches, sort by flight number, and return as JSON.
 }
 
+// Submit given launch data to launch system.
 async function httpSubmitLaunch(launch) {
   try {
     return await fetch(`${API_URL}/launches`, {
-      method: 'post',
+      method: "post",
       headers: {
-        "Content-Type": "application/json" 
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(launch)
-    }) 
-  } catch (error) {
+      body: JSON.stringify(launch),
+    });
+  } catch(err) {
     return {
-      ok: false
-    }
+      ok: false,
+    };
   }
-
-  // TODO: Once API is ready.
-  // Submit given launch data to launch system.
 }
 
+// Delete launch with given ID.
 async function httpAbortLaunch(id) {
   try {
     return await fetch(`${API_URL}/launches/${id}`, {
-      method: 'delete',
-    }) 
-  } catch (err) {
-    console.log(err)
+      method: "delete",
+    });
+  } catch(err) {
+    console.log(err);
     return {
-      ok: false
-    }
+      ok: false,
+    };
   }
-
-  // TODO: Once API is ready.
-  // Delete launch with given ID.
 }
 
 export {
--- Nasa_19110462/server/src/app.js
+++ Nasa_19110462/server/src/app.js
@@ -2,79 +2,23 @@ const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
-const request = require('supertest');
 
-const planetsRouter = require('./router/planets/planets.router') ;
-const launchesRouter = require('./router/launches/launches.router');
-
+const api = require('./routers/api');
 
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:3000",
+  origin: 'http://localhost:3000',
 }));
 app.use(morgan('combined'));
 
-
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '..' , 'public')));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
-app.use('/planets', planetsRouter);
-app.use('/launches', launchesRouter);
+app.use('/v1', api);
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
 
-//  Test GET planets
-request(app)
-  .get('/planets')
-  .expect('Content-Type', /json/)
-  .expect(200)
-  .end(function(err, res) {
-    if (err) throw err;
-  });
-
-
-
-//  Test GET launches
-request(app)
-  .get('/launches')
-  .expect('Content-Type', /json/)
-  .expect(200)
-  .end(function(err, res) {
-    if (err) throw err;
-  });
-
-
-
-//  Test POST launches
-request(app)
-    .post('/launches')
-    .send({
-            "mission": "ZTM155",
-            "rocket": "ZTM Experimental IS1",
-            "launchDate": 169148999999,
-            "target": "Minh Thang"
-        })
-    .set('Accept', 'application/json')
-    .expect('Content-Type', /json/)
-    .expect(201)
-    .end(function(err, res) {
-        if (err) throw err;
-    });
-
-
-//  Test DELETE launches
-request(app)
-    .delete(`/launches/${101}`)
-    .expect('Content-Type', /json/)
-    .expect(200)
-    .end(function(err, res) {
-        if (err) throw err;
-    });
- 
-
-
 module.exports = app;
-
